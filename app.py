94faf1416d35e1ec267a7f5e19f941ee2e11cc16
EC2 Default User <ec2-user@ip-172-31-28-172.ec2.internal>
2019-08-27 09:28:00 +0000

    Made first connection to the database and displayed placenames in placeNames.html

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'place_names_site'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:r00tUser@myfirstcluster-bhsq8.mongodb.net/place_names_site?retryWrites=true&w=majority')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_place_names')
+def get_place_names():
+    return render_template("placeNames.html", place_names=mongo.db.place_names.find())
 
 
 if __name__ == '__main__':
